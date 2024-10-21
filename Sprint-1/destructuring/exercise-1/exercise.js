b972e3ea14670e0230aecabf64b45476c7154f00
Daniel Wagner-Hall <daniel@codeyourfuture.io>
2024-10-21 10:48:46 +0100

    Rewrite "array destructuring" exercises as object destructuring (#47)
    These exercises do not actually benefit from array destructuring, so are
    quite confusing as-is.
    
    But they are great examples of object destructuring. Let's call them
    that.
    
    Also move them to a Sprint directiry, like all of the other modules are
    laid out.

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -4,6 +4,8 @@ const personOne = {
   favouriteFood: "Spinach",
 };
 
+// Update the parameter to this function to make it work.
+// Don't change anything else.
 function introduceYourself(___________________________) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
