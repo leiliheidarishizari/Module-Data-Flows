c288a951663525d6181b9ea3757e292e0cb005cf
leiliheidarishizari <leiliheidarishizari@gmail.com>
2024-12-17 20:48:56 +0000

    Completing Sprint-1/destructuring/excersice-3

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,19 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+console.log("QTY     ITEM                TOTAL");
+
+let totalCostPence = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const itemTotalPence = quantity * unitPricePence; // Calculate total for each item
+  totalCostPence += itemTotalPence; // Add to total cost
+
+  const itemTotal = (itemTotalPence / 100).toFixed(2); // Convert pence to pounds
+  console.log(
+    `${quantity.toString().padEnd(7)}${itemName.padEnd(20)}${itemTotal}`
+  );
+});
+
+const totalCost = (totalCostPence / 100).toFixed(2); // Convert total pence to pounds
+console.log(`\nTotal: ${totalCost}`);
